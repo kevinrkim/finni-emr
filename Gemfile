3d29097993710010ada76d62d25c2ff8e7807058
Kevin Kim <github@kevinckr.com>
2023-08-06 19:07:02 -0400

    add bootstrap for basic styling

--- Gemfile
+++ Gemfile
@@ -72,3 +72,5 @@ group :test do
 end
 
 gem "faker", "~> 3.2"
+
+gem "bootstrap", "~> 5.2"
---
+++ app/javascript/application.js
@@ -0,0 +1,7 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+//= require rails-ujs
+//= require activestorage
+//= require popper
+//= require bootstrap
+//= require_tree .
